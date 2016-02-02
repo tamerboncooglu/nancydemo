1ef73de409fa3e3e68ddfc9c5a3bb0b9dc96d516
Tamer Boncooglu <tamer.boncooglu@ciceksepeti.com>
2016-02-02 16:18:55 +0200

    first commit

---
+++ NancyDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.Owin.Hosting;
+
+namespace NancyDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var port = 8080;
+
+            using (WebApp.Start<Startup>(string.Format("http://+:{0}", port)))
+            {
+                Console.WriteLine("Nancy now listening on http://+:" + port);
+
+                var line = Console.ReadLine();
+                while (line != "quit")
+                {
+                    line = Console.ReadLine();
+                }
+            }
+        }
+    }
+
+
+}
